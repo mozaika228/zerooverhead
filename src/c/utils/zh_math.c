73143bbd80a4c7f15351db89ba19689cbc4b3d08
mozaika228 <mozaika228@gmail.com>
2026-03-11 22:30:30 +0500

    Fix MSVC pause intrinsic include

--- src/c/utils/zh_math.c
+++ src/c/utils/zh_math.c
@@ -1,8 +1,11 @@
 #include "../zh_internal.h"
 
+#if defined(_MSC_VER)
+  #include <intrin.h>
+#endif
+
 void zh_cpu_relax(void) {
 #if defined(_MSC_VER)
-  #include <immintrin.h>
   _mm_pause();
 #elif defined(__i386__) || defined(__x86_64__)
   __asm__ __volatile__("pause");
